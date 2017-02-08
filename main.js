f4334d96991eae3b77f6235f5a73d372827823f3
maniart <mani.art@gmail.com>
2017-02-08 12:30:22 -0500

    implement basic lang toggle

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 jQuery(document).ready(function($) {
 
-
+  $langToggle = $('.language-selector');
+  var letterData = undefined;
 
   $.ajax({
     url: './letter.json',
@@ -8,9 +9,45 @@ jQuery(document).ready(function($) {
       format: 'json'
     }
   }).done(function(data) {
-    console.log('done ', data)
+    window.letterData = letterData = data[0];
+    initLanguageToggle();
   }).fail(function(err) {
     console.error('Failed to fetch letters ', err);
   });
+
+  function setTitle(language) {
+    var $title = $('.title h2');
+    $title.text(letterData[language].title);
+  }
+
+  function setLetter(language) {
+    var $letter = $('.letter');
+    var $p;
+    $letter.empty();
+    letterData[language].text.forEach(function(paragraph) {
+      $p = $('<p/>');
+      $p.text(paragraph);
+      $letter.append($p)
+    });
+  }
+
+  function setContent(language) {
+    if (!letterData) {
+      return;
+    }
+    setTitle(language);
+    setLetter(language);
+  }
+
+
+  function initLanguageToggle() {
+    $langToggle.removeAttr('disabled');
+    $langToggle.on('change', function(e) {
+      e.preventDefault();
+      var targetLanguage = $(this).children(':selected').attr('value');
+      setContent(targetLanguage);
+      console.log(targetLanguage);
+    });
+  }
 
 });
