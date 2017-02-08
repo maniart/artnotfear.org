39455ab2da20660ac19dfeef8ffd9c3a0c8a1b86
maniart <mani.art@gmail.com>
2017-02-08 13:14:23 -0500

    more langs

--- main.js
+++ main.js
@@ -17,24 +17,33 @@ jQuery(document).ready(function($) {
 
   function setTitle(language) {
     var $title = $('.title h2');
-    $title.text(letterData[language].title);
+    $title.fadeOut(300, function() {
+      $title.text(letterData[language].title);
+      $title.fadeIn(300);
+    })
+
   }
 
   function setLetter(language) {
     var $letter = $('.letter');
     var $p;
-    $letter.empty();
-    letterData[language].text.forEach(function(paragraph) {
-      $p = $('<p/>');
-      $p.text(paragraph);
-      $letter.append($p)
+    $letter.fadeOut(300, function() {
+      $letter.empty();
+      letterData[language].text.forEach(function(paragraph) {
+        $p = $('<p/>');
+        $p.text(paragraph);
+        $letter.append($p)
+      });
+      $letter.fadeIn(300);
     });
+
   }
 
   function setContent(language) {
     if (!letterData) {
       return;
     }
+    window.scroll(0,0);
     setTitle(language);
     setLetter(language);
   }
@@ -42,6 +51,7 @@ jQuery(document).ready(function($) {
 
   function initLanguageToggle() {
     $langToggle.removeAttr('disabled');
+    // initially
     $langToggle.on('change', function(e) {
       e.preventDefault();
       var targetLanguage = $(this).children(':selected').attr('value');
@@ -49,5 +59,6 @@ jQuery(document).ready(function($) {
       console.log(targetLanguage);
     });
   }
+
 
 });
